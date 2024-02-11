5fddc4eb42cf6b46f109a9a751aaee585fd0c5c3
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-02-10 21:10:45 -0700

    Code fininshed

--- ConvertAndValidate.vb
+++ ConvertAndValidate.vb
@@ -1,10 +1,51 @@
-﻿Option Strict On
-Option Explicit On
-
+﻿
 Module ConvertAndValidate
 
     Sub Main()
 
+        'for testing the function add this to your Sub Main()
+
+        Dim aValidNumber As Integer
+        Dim usernResponse As String
+
+        Do
+            Console.WriteLine($"Enter a number:")
+
+            usernResponse = Console.ReadLine()
+            aValidNumber = CInt(usernResponse)
+
+            If ConversionValid(usernResponse, aValidNumber) = True Then
+
+                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
+
+            Else
+
+                Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+
+            End If
+
+        Loop Until ConversionValid(usernResponse, aValidNumber) = True
+
+        Console.Read()
+
     End Sub
+
+    'make this work
+
+
+    Function ConversionValid(convertThisString, toThisInteger) As Boolean
+
+        Dim status As Boolean
+
+        Try
+            toThisInteger = CInt(convertThisString)
+            status = True
+        Catch ex As Exception
+            status = False
+        End Try
+
+        Return status
+
+    End Function
 
 End Module
